7f196cab34564fcb3bafe256e1efc624aab56632
kleclaire <kelvin.leclaire@student.fh-kiel.de>
2020-03-22 14:34:08 +0100

    Home gestaltet und Favicon geändert

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Component } from 'react';
+import logo from './wirvsvirus.png';
 
 export class Home extends Component {
     static displayName = Home.name;
@@ -7,54 +8,18 @@ export class Home extends Component {
     public render(): React.ReactElement {
         return (
             <div>
-                <h1>Hello, world!</h1>
-                <p>Welcome to your new single-page application, built with:</p>
-                <ul>
-                    <li>
-                        <a href="https://get.asp.net/">ASP.NET Core</a> and{' '}
-                        <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">
-                            C#
-                        </a>{' '}
-                        for cross-platform server-side code
-                    </li>
-                    <li>
-                        <a href="https://facebook.github.io/react/">React</a>{' '}
-                        for client-side code
-                    </li>
-                    <li>
-                        <a href="http://getbootstrap.com/">Bootstrap</a> for
-                        layout and styling
-                    </li>
-                </ul>
-                <p>To help you get started, we have also set up:</p>
-                <ul>
-                    <li>
-                        <strong>Client-side navigation</strong>. For example,
-                        click <em>Counter</em> then <em>Back</em> to return
-                        here.
-                    </li>
-                    <li>
-                        <strong>Development server integration</strong>. In
-                        development mode, the development server from{' '}
-                        <code>create-react-app</code> runs in the background
-                        automatically, so your client-side resources are
-                        dynamically built on demand and the page refreshes when
-                        you modify any file.
-                    </li>
-                    <li>
-                        <strong>Efficient production builds</strong>. In
-                        production mode, development-time features are disabled,
-                        and your <code>dotnet publish</code> configuration
-                        produces minified, efficiently bundled JavaScript files.
-                    </li>
-                </ul>
-                <p>
-                    The <code>ClientApp</code> subdirectory is a standard React
-                    application based on the <code>create-react-app</code>{' '}
-                    template. If you open a command prompt in that directory,
-                    you can run <code>npm</code> commands such as{' '}
-                    <code>npm test</code> or <code>npm install</code>.
-                </p>
+                <h1>Hello und herzlich Willkommen!</h1>
+                <h5>Dies ist eine WirVsVirus Website zur Digitalen Krankheuts-Anamnese.</h5>
+                <p> </p>
+                <p>Diese Seite soll zur digitalen Erfassung von Krankheutsverläufen potentieller Erkrankter dienen.</p>
+                <p>Sie können sich hier registrieren, oder wenn Sie dies schon haben, anmelden und mehrmals täglich Ihre Symptome und mehr an Ihr zuständiges Gesundheitsamt senden.
+                Somit stellen wir zum einen Sicher, dass sie die best mögliche Betreuung erhalten, aber auch die momentan überlasteten Gesundheitsämter
+                etwas entlastet werden.</p>
+                <p>Sollten Sie nicht genau wissen, welchem Gesundheitsamt sie zugeordnet sind, können Sie dies problemlos <a href="https://tools.rki.de/PLZTool/" target="_blank" >hier</a> überprüfen.</p>
+                <p>Diese Seite ist im Rahmen des #wirvsvirus Hackerthon entstanden.</p>
+                <img src={logo} alt="logo" />
+
+
             </div>
         );
     }
